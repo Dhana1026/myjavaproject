6e260c73412f9051e4e51088320f58b670bc4019
dh <dh.com>
2023-10-06 15:26:55 +0530

    hiwww

--- myjava.java
+++ myjava.java
@@ -1,5 +1,5 @@
 class Simple{  
 public static void main(String args[]){  
-System.out.println("Hello Java hoy kka mkndsnknkshhd");  
+System.out.println("Hello Java hoy kka mkndsnknkshhd khinndhhd");  
 }  
 } 
